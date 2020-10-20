6d9fab231b5a7ee279275a43f172c35f47aa5a6c
Christian Wiegand <cwiegand@wgdnet.de>
2020-10-20 17:01:40 +0200

    Fix breadcrumbs template to bootstrap4

--- django_restic_gui/settings.py
+++ django_restic_gui/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'repository',
+    'django_restic_gui',
     'django_bootstrap_breadcrumbs',
     'bootstrap4',
 ]

ee95b3e42c678f786b18d90b63345464c2219009
Christian Wiegand <cwiegand@wgdnet.de>
2020-12-03 18:29:39 +0100

    Calc size of backups

--- repository/views.py
+++ repository/views.py
@@ -33,12 +33,39 @@ def restic_command(repo, command):
     return subprocess.run(command, stdout=subprocess.PIPE, env=my_env)
 
 
+def get_directory_size(directory):
+    """Returns the `directory` size in bytes."""
+    total = 0
+    try:
+        # print("[+] Getting the size of", directory)
+        for entry in os.scandir(directory):
+            if entry.is_file():
+                # if it's a file, use stat() function
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                # if it's a directory, recursively call this function
+                total += get_directory_size(entry.path)
+    except NotADirectoryError:
+        # if `directory` isn't a directory, get the file size then
+        return os.path.getsize(directory)
+    except PermissionError:
+        # if for whatever reason we can't open the folder, return 0
+        return 0
+    return total
+
+
 class RepositoryList(LoginRequiredMixin, ListView):
     model = Repository
 
     def get(self, request, *args, **kwargs):
         clear()
         return super(RepositoryList, self).get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = Repository.objects.all()
+        for repo in qs:
+            repo.size = humanize.naturalsize(get_directory_size(repo.path), binary=True)
+        return qs
 
     def get_context_data(self, *, object_list=None, **kwargs):
         ctx = super(RepositoryList, self).get_context_data(**kwargs)
@@ -55,7 +82,6 @@ class RepositoryList(LoginRequiredMixin, ListView):
         else:
             ctx['bar_class'] = 'bg-danger'
         return ctx
-
 
 
 class RepositoryUpdate(LoginRequiredMixin, SuccessMessageMixin, UpdateView):

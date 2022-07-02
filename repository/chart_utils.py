f3451e1764b7e36a821a4e47fcf0d8c8a0d4c238
Mario Orlandi <morlandi@brainstorm.it>
2022-07-02 12:26:40 +0200

    Using a separate chart for every Repo

--- repository/chart_utils.py
+++ repository/chart_utils.py
@@ -5,31 +5,33 @@ from repository.models import RepoSize
 
 def repo_datasets(index, repo):
 
-    def color_code(index, opacity):
-        h = ((index * 60) % 360) / 360.0
-        l = s = 0.9
-        r, g, b = colorsys.hsv_to_rgb(h, l, s)
-        return "rgba(%d,%d,%d,%.1f)" % (int(r * 255), int(g * 255), int(b * 255), opacity)
+    # def color_code(index, opacity):
+    #     h = ((index * 60) % 360) / 360.0
+    #     l = s = 0.9
+    #     r, g, b = colorsys.hsv_to_rgb(h, l, s)
+    #     return "rgba(%d,%d,%d,%.1f)" % (int(r * 255), int(g * 255), int(b * 255), opacity)
 
-    fg = color_code(index, 1.0)
-    bg = color_code(index, 0.1)
+    # fg = color_code(index, 1.0)
+    # bg = color_code(index, 0.1)
+    fg = "#007bff"
+    bg = "#007bff"
 
     datasets = [{
-        'label': '"%s" %s' % (repo.name, _('size [GB]')),
+        'label': str(_('size [GB]')),
         'yAxisID': 'y1',
         'fill': False,
-        'pointRadius': 3.0,
+        'pointRadius': 2.0,
         'borderColor': fg,
         'pointBackgroundColor': fg,
         'backgroundColor': bg,
         'data': [],
     }, {
-        'label': '"%s" %s' % (repo.name, _('files count')),
+        'label': str(_('K files')),
         'yAxisID': 'y2',
         'fill': False,
         'pointRadius': 2.0,
-        'borderDash': [1, 3],
         'borderColor': fg,
+        'borderDash': [1, 1],
         'pointBackgroundColor': fg,
         'backgroundColor': bg,
         'data': [],
@@ -47,7 +49,7 @@ def repo_datasets(index, repo):
         })
         datasets[1]['data'].append({
             'x': row.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-            'y': row.file_count
+            'y': row.file_count / 1000
         })
 
     return datasets, time_unit
